df1ca31a0d689cb42ce173ded31d5c22ad0ce8e2
morapelker <morapelker@gmail.com>
2022-07-07 22:25:49 +0300

    "v1.0.2"

--- TedoooPremiumApi/Classes/TedoooPremiumApi.swift
+++ TedoooPremiumApi/Classes/TedoooPremiumApi.swift
@@ -5,8 +5,19 @@ public enum PremiumPlan {
     case yearly
 }
 
+public struct PremiumPerson {
+    public init(name: String, avatar: String) {
+        self.name = name
+        self.avatar = avatar
+    }
+    
+    public let name: String
+    public let avatar: String
+}
+
 public protocol TedoooPremiumApi {
     
     func startBillingProcess(presentor: UIViewController, plan: PremiumPlan) -> AnyPublisher<Int64, Never>
         
+    func getPremiumPeople() -> AnyPublisher<[PremiumPerson], Never>
 }
